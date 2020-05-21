7a26d1af7a389430f187f4497b3f677f6434c0a9
Jeferson <mx,jeferson.12@gmail.com>
2020-05-21 15:11:45 -0300

    1.0

--- script.js
+++ script.js
@@ -2,19 +2,11 @@ const box = document.querySelector('.box')
 const button = document.querySelector('#copiar')
 const ranges = document.querySelectorAll('input[type=range]')
 
-let valores = {}
-
-function handleInput(e, direction){
-    return e.target.id == direction ? e.target.value : e.target.value
-}
-
 ranges.forEach(range => {
-    range.addEventListener("change", e => {
-        box.style.borderRadius = `
-            ${handleInput(e, "topo")}em
-            ${handleInput(e, "esquerdo")}em
-            ${handleInput(e, "direito")}em
-            ${handleInput(e, "tarde")}em
-        `
+    range.addEventListener('input', e => {
+        document.documentElement.style.setProperty(
+            `--${e.target.name}`,
+            e.target.value
+        )
     })
-})
+});
